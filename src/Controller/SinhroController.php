99dacf04df269b1da7e7aacebfac6cb33a4ade43
IgorUU <igor_1991@live.com>
2021-11-08 21:03:29 +0100

    Izmene

--- src/Controller/SinhroController.php
+++ src/Controller/SinhroController.php
@@ -7,7 +7,7 @@ use Drupal\node\Entity\Node;
 
 class SinhroController extends ControllerBase {
 
-    public function newpage() {
+    public function posts() {
         $data = file_get_contents('https://jsonplaceholder.typicode.com/posts');
         $posts = json_decode($data, TRUE);
         // dsm($posts);
@@ -38,7 +38,54 @@ class SinhroController extends ControllerBase {
         }
 
         return [
-            '#title' => 'Updejtovan kontent'
+            '#markup' => '<h1>Updejtovani postovi! </h1>'
+        ];
+    }
+
+    public function tasks() {
+        $data = file_get_contents('https://jsonplaceholder.typicode.com/todos');
+        $tasks = json_decode($data, TRUE);
+        //dsm($tasks); //userId, id, title, completed
+
+        foreach($tasks as $task) {
+            $values = \Drupal::entityQuery('node')->condition('field_task_id', $task['id'])->execute();
+            $node_not_exists = empty($values);              //Proveravamo da li već postoji node gde se 'field_task_id' i $task['id'] poklapaju.
+
+            $query = \Drupal::entityQuery('user');
+            $uids = $query->execute();
+            $assigned = '';
+
+            if($node_not_exists) {
+
+                // if(in_array($task['userId'], $uids)){
+                //     $assigned = 'To this user';
+                // } else {
+                //     $assigned = 'Assigned to administrator';
+                // };
+
+                $node = \Drupal::entityTypeManager()->getStorage('node')->create([
+                    'type' => 'tasks',
+                    'field_user' => $task['userId'],
+                    'field_task_id' => $task['id'],
+                    'title' => $task['title'],
+                    'field_completed' => $task['completed'],
+                    'field_assigned' => $assigned
+                ]);
+                $node->save();
+            } else {
+                $nid = reset($values);
+
+                $node = Node::load($nid);
+                $node->set('title', $task['title']);
+                $node->set('field_task_id', $task['id']);
+                $node->set('field_user', $task['userId']);
+                $node->set('field_completed', $task['completed']);
+                $node->set('field_assigned', $assigned);
+                $node->save();
+            }
+        }
+        return [
+            '#markup' => '<h1>Updejtovani taskovi! </h1>'
         ];
     }
     
